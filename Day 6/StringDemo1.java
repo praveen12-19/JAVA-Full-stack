eb4ef20df2f05a376d44ccadf5f2c488462342d4
Praveen <ppk763957@gmail.com>
2025-12-17 10:01:05 +0530

    Resources added

---
+++ Day 6/StringDemo1.java
@@ -0,0 +1,26 @@
+
+public class StringDemo1 {
+    public static void main(String[] args) {
+        
+        //indexOf()
+        String s ="I like Biriyani";
+        System.out.println(s.indexOf("y"));
+
+        //indexOf(with index)
+        System.out.println(s.indexOf("a", 4));
+
+        //lastIndex()
+        System.out.println(s.lastIndexOf("i"));
+
+        //substring(start index,end index-1)
+        String str="My laptop";
+        System.out.println(str.substring(0, 6));
+
+        //
+
+
+
+
+    }
+    
+}
